047ed3e2e9aef366880c7be311ca833076a7013a
Aredhele <vincentstehly@hotmail.fr>
2018-01-27 00:26:59 +0100

    Add TimeManager

---
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Stores frame time
+ * @class TimeManager
+ */
+public class TimeManager : MonoBehaviour
+{
+    public static float deltaTime;
+    public static float timeScale;
+
+    /**
+     * Called at start
+     */
+	void Start ()
+    {
+        TimeManager.deltaTime = 0.0f;
+        TimeManager.timeScale = 1.0f;
+    }
+	
+    /**
+     * Called each update
+     */
+	void Update ()
+    {
+        TimeManager.deltaTime = TimeManager.timeScale * Time.deltaTime;
+    }
+}
