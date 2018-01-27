4015da2537b99254ed4e342dfdff4e9aa8ffb76c
Aredhele <vincentstehly@hotmail.fr>
2018-01-27 03:24:09 +0100

    Add skill scene

---
+++ Assets/Scripts/Controllers/UI/AnglePickerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Draws a picker to select the 
+ * angle to cast the wave toward to
+ * @class AnglePickerController
+ */
+public class AnglePickerController : MonoBehaviour
+{
+    /**
+     * Called at start
+     */
+	void Start ()
+    {
+		
+	}
+	
+    /**
+     * Called each update
+     */
+	void Update ()
+    {
+		
+	}
+}
