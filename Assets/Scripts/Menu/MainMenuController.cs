c9ade0a132a4cb677f16dd5620f4dd57830cf3a7
Aredhele <vincentstehly@hotmail.fr>
2018-01-28 16:32:53 +0100

    Fix menu

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -8,21 +8,25 @@ public class MainMenuController : MonoBehaviour
 {
     public void OnPlay()
     {
+        AkSoundEngine.PostEvent("UI_Chose", this.gameObject);
         SceneManager.LoadSceneAsync("Main");
     }
 
     public void OnCreditOpen()
     {
+        AkSoundEngine.PostEvent("UI_Chose", this.gameObject);
         SceneManager.LoadSceneAsync("CreditScreen");
     }
 
     public void OnGameExit()
     {
+        AkSoundEngine.PostEvent("UI_Chose", this.gameObject);
         Application.Quit();
     }
 
     public void OnMainMenuBack()
     {
+        AkSoundEngine.PostEvent("UI_Chose", this.gameObject);
         SceneManager.LoadSceneAsync("TitleScreen");
     }
 }
